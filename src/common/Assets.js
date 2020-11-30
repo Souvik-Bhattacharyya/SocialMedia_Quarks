241302fd8c37f3ba221399106a03fe49d58334d9
hat-ad <dasashis672@gmail.com>
2020-11-30 20:37:20 +0530

    setup

--- src/common/Assets.js
+++ src/common/Assets.js
@@ -1,7 +1,15 @@
+import React from "react";
+import AddIcon from '@material-ui/icons/Add';
+
+
 const Assets = {
   images: {
     logo: require("../Assets/images/logo512.png"),
-  }
+  },
+  icons: {
+    add : AddIcon,
+    
+  },
 };
 
 export default Assets;
